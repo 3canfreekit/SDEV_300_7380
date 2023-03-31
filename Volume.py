68bb3190076b8b47550b7d4b929d0652a5375de1
Michael Bennett <86172375+3canfreekit@users.noreply.github.com>
2023-03-31 16:18:52 -0400

    Add files via upload

---
+++ Volume.py
@@ -0,0 +1,29 @@
+##MICHAEL_BENNETT
+##SDEV300_7380
+##Professor_Zachery_Fair
+##Week2_assignment2_Calculate_the_Volume_of_a_Right_Circular_Cylinder6
+##3/28/2023
+
+# Program to calculate the volume of a right circular cylinder
+
+# Importing math library
+import math
+
+# Taking the radius and height of the cylinder as input from the user
+radius = float(input("Enter radius of the cylinder ( cm): "))
+height = float(input("Enter height of the cylinder ( cm): "))
+
+# Calculating the volume of the cylinder using the formula given
+volume = math.pi * radius * radius * height
+
+# Printing the result
+print("\nVolume of the Cylinder =", volume, "cubic centimeter")
+
+# Process and Calculations:
+# Formula used: Volume of Circular Cylinder = (πr2) × Henormula 'Volume of Circular Cylinder = (πr2) × Height'
+# Printing the result
+
+# Details:
+# Radius of the Cylinder: _____ cm
+# Height of the Cylinder: _____ cm
+# Volume of the Cylinder: _____ cubic centimeter
